3ce124d6bece622d06e901adf135b37fb768229b
shahnavaz <shahnavaz.pathan@upstrapp.com>
2022-12-15 20:33:59 +0530

    update

--- air_chatbot/settings.py
+++ air_chatbot/settings.py
@@ -124,10 +124,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-ALLOWED_HOSTS =["*"]
-
-# CORS_ALLOWED_ORIGINS = ['*']
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
